a9b010dcd7a883208cb97810ae9120be472a8dd6
nirawadea <nirawade.a@northeastern.edu>
2024-12-02 15:58:52 -0500

    A9-1 changes

--- src/main/java/awslambda/EmailVerificationLambda.java
+++ src/main/java/awslambda/EmailVerificationLambda.java
@@ -11,17 +11,32 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 import java.util.Map;
 
 public class EmailVerificationLambda implements RequestHandler<SNSEvent, String> {
 
     private static final long EXPIRATION_TIME = 2 * 60 * 1000; // 2 minutes in milliseconds
-    private final String sendgridApiKey = "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";
+//    private final String sendgridApiKey = "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";
+
+//    @Value("${aws.secret.sendgrid.api.key}")
+//    private String sendGridApi;
+
 
     @Override
     public String handleRequest(SNSEvent event, Context context) {
         context.getLogger().log("Lambda function invoked with SNS event");
+        // Retrieve secret name from environment variables
+        String secretName = System.getenv("SEND_GRID_SECRET_NAME");
+
+        // Retrieve the secret from AWS Secrets Manager
+        Map<String, String> secrets = getSecret(secretName,context);
+        // Extract the SendGrid API key from the retrieved secret
+        String sendgridApiKey = secrets.get("api_key");
 
         // Log environment variables for debugging (remove in production)
         context.getLogger().log("sendgridApiKey: " + sendgridApiKey);
@@ -44,7 +59,7 @@ public class EmailVerificationLambda implements RequestHandler<SNSEvent, String>
             context.getLogger().log("Generated verification link: " + verificationLink);
 
             // Send verification email
-            sendEmail(email, verificationLink, context);
+            sendEmail(email, verificationLink, context, sendgridApiKey);
 
             return "Email sent successfully!";
         } catch (Exception e) {
@@ -56,10 +71,10 @@ public class EmailVerificationLambda implements RequestHandler<SNSEvent, String>
 
     private String generateVerificationLink(String email, String userId) {
         long expirationTime = System.currentTimeMillis() + EXPIRATION_TIME;
-        return "http://dev.aayukacloudnative.me/v1/user/verify?token=" + userId + "&email=" + email + "&expires=" + expirationTime;
+        return "https://dev.aayukacloudnative.me/v1/user/verify?token=" + userId + "&email=" + email + "&expires=" + expirationTime;
     }
 
-    private void sendEmail(String email, String verificationLink, Context context) throws Exception {
+    private void sendEmail(String email, String verificationLink, Context context, String sendgridApiKey) throws Exception {
         Email from = new Email("nirawade.a@northeastern.edu");
         String subject = "Email Verification";
         Email to = new Email(email);
@@ -79,6 +94,23 @@ public class EmailVerificationLambda implements RequestHandler<SNSEvent, String>
         if (response.getStatusCode() != 202) {
             context.getLogger().log("Failed to send email: " + response.getBody());
             throw new Exception("Failed to send email: " + response.getBody());
+        }
+    }
+
+    public Map<String, String> getSecret(String secretName, Context context) {
+        SecretsManagerClient secretsManagerClient = SecretsManagerClient.builder().region(Region.of("us-east-1")).build();
+        GetSecretValueRequest secretValueRequest = GetSecretValueRequest.builder().secretId(secretName).build();
+
+        GetSecretValueResponse secretValueResponse;
+
+        // Parse secret value as JSON
+        try {
+            secretValueResponse = secretsManagerClient.getSecretValue(secretValueRequest);
+            ObjectMapper objectMapper = new ObjectMapper();
+            context.getLogger().log("SecretString: " + secretValueResponse.secretString());
+            return objectMapper.readValue(secretValueResponse.secretString(), Map.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse secret value", e);
         }
     }
 }
